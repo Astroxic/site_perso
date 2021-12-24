73e27174c0f96fe1ee5581b58711b1b47374f21c
Vin's <vincent.pilard@sts-sio-caen.info>
2021-12-24 03:52:09 +0100

    oui

--- php/contact.php
+++ php/contact.php
@@ -12,7 +12,7 @@ foreach ($data[0] as $elt) {
 }
 echo '<h3>Contenu du message*</h3>';
 echo '<textarea rows="15" class="barre-grosse" name="commentaire" placeholder="Entrez le contenu du message" required="required" style=resize:none></textarea><br>';
-echo '<div class="g-recaptcha" data-sitekey="6LcTK7sdAAAAAH6onQeZRqdEdRKauS9RbrMDp2Sk"></div>';
+echo '<div class="g-recaptcha" data-theme="dark" data-sitekey="6LcTK7sdAAAAAH6onQeZRqdEdRKauS9RbrMDp2Sk"></div>';
 echo '<input class="submit" type="submit" value="Soumettre">';
 echo '<p id="formupara">Les informations portées sur ce formulaire font l’objet d’un traitement informatisé destiné à envoyer un email.<br> Le destinataire des données est <b>Vincent</b></p>';
 echo '</form>';
